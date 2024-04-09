6649946cf4b15e285f57b8c14319d19d03863489
Martijn Hiemstra <info@greenhighway.nl>
2024-04-09 16:11:14 +0200

    Kleine aanpassing

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ print('Start api read application')
 import requests
 
 paginaresults = requests.get('https://catfact.ninja/facts')
-print(paginaresults)
+# print(paginaresults)
 
 feitjes = paginaresults.json()
 
